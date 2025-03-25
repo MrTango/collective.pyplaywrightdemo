5907b8a9e611ffa358b8f0cd0d50cd1ba0414d57
MrTango <md@derico.de>
2025-03-25 12:42:03 +0200

    cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,37 +43,6 @@ def generate_basic_authentication_header_value(username: str, password: str) -> 
     return "Basic {}".format(token)
 
 
-@pytest.fixture(scope="session", autouse=True)
-def generate_mo():
-    """Generate .mo files."""
-    import collective.pyplaywrightdemo
-
-    locales_path = Path(collective.pyplaywrightdemo.__file__).parent / "locales"
-    po_files: Generator = locales_path.glob("**/*.po")
-    for po_file in po_files:
-        parent: Path = po_file.parent
-        domain: str = po_file.name[: -len(po_file.suffix)]
-        mo_file: Path = parent / f"{domain}.mo"
-        try:
-            mo = Msgfmt(f"{po_file}", name=domain).getAsFile()
-        except PoSyntaxError:
-            continue
-        else:
-            with open(mo_file, "wb") as f_out:
-                f_out.write(mo.read())
-
-
-@pytest.fixture()
-def plone_url():
-    """"""
-    ZOPE_HOST = WSGI_SERVER_FIXTURE.host
-    ZOPE_PORT = WSGI_SERVER_FIXTURE.port
-    ZOPE_URL = f"http://{ZOPE_HOST}:{ZOPE_PORT}"
-    PLONE_SITE_ID = "plone"
-    PLONE_URL = f"{ZOPE_URL}/{PLONE_SITE_ID}"
-    return PLONE_URL
-
-
 @pytest.fixture()
 def portal_factory(acceptance ,request):
     def factory(roles: list, username: str = TEST_USER_NAME):
